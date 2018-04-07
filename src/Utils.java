3b72ceb928716e194c4083c7684f7b50d0b3fe2c
caleb logan <clogan202@gmail.com>
2018-04-07 13:05:19 -0700

    add method to get mac address

--- src/Utils.java
+++ src/Utils.java
@@ -15,10 +15,29 @@ public class Utils {
     }
 
     public static String getMACAddress() {
-        return "123";
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+            byte[] mac = network.getHardwareAddress();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < mac.length; i++) {
+                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+            }
+            return sb.toString();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (SocketException e){
+            e.printStackTrace();
+        }
+        return "";
     }
 
     public static String getPID() {
         return ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getClientID());
+        System.out.println(ManagementFactory.getRuntimeMXBean().getName());
     }
 }
