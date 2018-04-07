da46d46ae4c48b2a09c3742dd75cef67791eecff
caleb logan <clogan202@gmail.com>
2018-04-06 20:15:19 -0700

    refactor magic strings

--- src/main.java
+++ src/main.java
@@ -1,12 +1,10 @@
 import org.dreambot.api.methods.Calculations;
-import org.dreambot.api.methods.interactive.Players;
 import org.dreambot.api.methods.map.Tile;
-import org.dreambot.api.script.AbstractScript;  // All these imports are from the dreambot API
-import org.dreambot.api.script.ScriptManifest;  // API can be found here: https://dreambot.org/javadocs/
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.ScriptManifest;
 import org.dreambot.api.script.Category;
 import org.dreambot.api.script.listener.MessageListener;
 import org.dreambot.api.utilities.Timer;
-import org.dreambot.api.wrappers.interactive.GameObject;
 import org.dreambot.api.wrappers.interactive.Player;
 import org.dreambot.api.wrappers.widgets.message.Message;
 
@@ -23,6 +21,12 @@ import java.util.Random;
 @ScriptManifest(author = "okc", name = "Chat Logger", version = 0.1, description = "Streaming chat", category = Category.UTILITY)
 public class main extends AbstractScript implements MessageListener {
 
+    String API_BASE = "http://localhost:8000/api/v1";
+    String API_MESSAGES = API_BASE + "/messages";
+
+    String botLocationName = "Varrock Ge";
+    int world = 301;
+
     Timer timer;
     Timer playerTimer;
     long nextPlayerUpdate;
@@ -31,30 +35,9 @@ public class main extends AbstractScript implements MessageListener {
 
     @Override
     public void onStart() {
-        log("Chat Logger!");	 // This will display in the CMD or logger on dreambot
+        log("Chat Logger!");
         timer = new Timer();
         playerTimer = new Timer();
-//        try {
-//            HashMap<String, Object> data = new HashMap<>();
-//            data.put("username", "xxbillyxx");
-//            data.put("message", "Just hit 99 mage!! Wooot!!");
-//            data.put("type", "game");
-//            data.put("tile", "127 333 0");
-//            data.put("locationName", "Varrock Ge");
-//            data.put("world", 1);
-//            data.put("created_at", 29324);
-//            HttpClient req = new HttpClient("http://localhost:8000/api/v1/messages/");
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(req.post(data)));
-//
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                System.out.println("line:" + line);
-//            }
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
 
     @Override
@@ -73,11 +56,11 @@ public class main extends AbstractScript implements MessageListener {
 
     @Override
     public void onExit() {
-        log("Stopping!"); // this will display in the CMD or logger on dreambot
+        log("Stopping!");
     }
 
     private void updatePlayerPositions() {
-        log("update player positions");
+        log("updating player positions");
         playerPositions = new HashMap<>();
         List<Player> players = getPlayers().all();
         for (Player player : players) {
@@ -102,16 +85,11 @@ public class main extends AbstractScript implements MessageListener {
                 Tile pos = playerPositions.get(message.getUsername());
                 data.put("tile", pos.getX() + " " + pos.getY() + " " + pos.getZ());
             }
-            data.put("locationName", "Varrock Ge");
-            data.put("world", 301);
+            data.put("locationName", botLocationName);
+            data.put("world", world);
 //            data.put("created_at", message.getTime());
-            HttpClient req = new HttpClient("http://localhost:8000/api/v1/messages/");
+            HttpClient req = new HttpClient(API_MESSAGES);
             BufferedReader reader = new BufferedReader(new InputStreamReader(req.post(data)));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println("line:" + line);
-            }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
